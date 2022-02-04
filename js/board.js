6beff445f03210384865c6d9146f83f471949003
Rohampingvin <petike804@gmail.com>
2022-02-04 09:06:13 +0100

    board_create,board_delete

--- js/board.js
+++ js/board.js
@@ -1,4 +1,4 @@
-let boards, valami;
+let boards, create, Delete;
 
 let getName = fetch(`https://api.trello.com/1/members/me/boards?key=${key}&token=${token}`, {
     method: 'GET',
@@ -6,6 +6,8 @@ let getName = fetch(`https://api.trello.com/1/members/me/boards?key=${key}&token
         {
             'Accept': 'application/json'
         }}).then(r => r.json()).then(d => boards = d);
+
+document.querySelector('#board_name').value = '';
 
 let allFetch = Promise.all([getName]);
 
@@ -16,10 +18,27 @@ function show(){
         let card = document.importNode(template, true);
         card.querySelector('.card-img').src = item.prefs.backgroundImage;
         card.querySelector('.card-title').innerHTML = item.name;
-        card.querySelector('.card').addEventListener("click", () => window.location.href = `https://api.trello.com/1/boards/${item.id}/lists?key=${key}&token=${token}`)
+        //card.querySelector('.card').addEventListener("click", () => window.location.href = `https://api.trello.com/1/boards/${item.id}/lists?key=${key}&token=${token}`)
+        card.querySelector('.delete').onclick = async function(){
+            fetch(`https://api.trello.com/1/boards/${item.id}?key=${key}&token=${token}`, {
+                method: 'DELETE'
+            });
+        }
         document.querySelector('.content').append(card);
     })
 }
 
+function board_create(){
+    let board_name = document.querySelector('#board_name');
+    
+    if (board_name.value.length > 1) {
+        create = fetch(`https://api.trello.com/1/boards/?key=${key}&token=${token}&name=${board_name.value}`, {
+            method: 'POST'
+            });
+            
+    }else{
+        alert('Adjon meg egy karaktert!!!!!444!!!!')
+    }
+}
 //https://api.trello.com/1/boards/{id}
 //https://api.trello.com/1/members/me/boards?fields=name,url&key={key}&token={token}
