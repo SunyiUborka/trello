4870ea68894a53a8b7590dace9c5fcfd6cfa8d25
Rohampingvin <petike804@gmail.com>
2022-02-07 23:05:40 +0100

    get lista

--- js/board.js
+++ js/board.js
@@ -1,2 +1,19 @@
 let boardId = window.location.search.split('=')[1];
 console.log(boardId)
+
+let lists;
+
+let getLists = fetch(`https:api.trello.com/1/boards/${boardId}/lists?key=${key}&token=${token} `, {
+    method: 'get'}).then(r => r.json()).then(d => lists = d);
+
+
+    let listall = Promise.all([getLists]);
+    listall.then(()=>show());
+function show() {
+    let template = document.querySelector("template").content;
+    lists.forEach(item => {
+        let list = document.importNode(template, true);
+        list.querySelector('.card-title').innerHTML = item.name;
+        document.querySelector('.content').append(list);
+    });
+}
